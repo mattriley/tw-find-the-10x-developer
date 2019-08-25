fb27fde93d9e8a5e527a45f71a21075e282292a4
Matthew Riley <matthew@matthewriley.name>
2019-08-25 16:11:22 +1000

    implement 'better than' predicate

--- src/predicates.js
+++ src/predicates.js
@@ -1,4 +1,5 @@
 module.exports = {
     notBest: a => arr => arr[0] !== a,
-    notWorst: a => arr => arr[arr.length - 1] !== a
+    notWorst: a => arr => arr[arr.length - 1] !== a,
+    betterThan: (a, b) => arr => arr.indexOf(a) < arr.indexOf(b)
 };
--- tests/predicates.test.js
+++ tests/predicates.test.js
@@ -1,6 +1,6 @@
 const test = require('tape');
 const predicates = require('../src/predicates');
-const { notBest, notWorst } = predicates;
+const { notBest, notWorst, betterThan } = predicates;
 
 test('not the best', t => {
     const arr = ['foo', 'bar'];
@@ -13,5 +13,13 @@ test('not the worst', t => {
     const arr = ['foo', 'bar'];
     t.true(notWorst('foo')(arr), 'foo is not the worst');
     t.false(notWorst('bar')(arr), 'bar is the worst');
+    t.end();
+});
+
+test('better than', t => {
+    const arr = ['foo', 'bar', 'baz'];
+    t.true(betterThan('foo', 'bar')(arr), 'foo is better than bar');
+    t.true(betterThan('foo', 'baz')(arr), 'foo is better than baz');
+    t.false(betterThan('bar', 'foo')(arr), 'bar is worse than foo');
     t.end();
 });
