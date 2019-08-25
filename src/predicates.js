416631666ed075cb1099d9cbdf6c76704105833c
Matthew Riley <matthew@matthewriley.name>
2019-08-25 16:14:12 +1000

    implement 'not directly above' predicate

--- src/predicates.js
+++ src/predicates.js
@@ -1,5 +1,6 @@
 module.exports = {
     notBest: a => arr => arr[0] !== a,
     notWorst: a => arr => arr[arr.length - 1] !== a,
-    betterThan: (a, b) => arr => arr.indexOf(a) < arr.indexOf(b)
+    betterThan: (a, b) => arr => arr.indexOf(a) < arr.indexOf(b),
+    notDirectlyAbove: (a, b) => arr => arr[arr.indexOf(b) - 1] !== a
 };
--- tests/predicates.test.js
+++ tests/predicates.test.js
@@ -1,6 +1,6 @@
 const test = require('tape');
 const predicates = require('../src/predicates');
-const { notBest, notWorst, betterThan } = predicates;
+const { notBest, notWorst, betterThan, notDirectlyAbove } = predicates;
 
 test('not the best', t => {
     const arr = ['foo', 'bar'];
@@ -21,5 +21,12 @@ test('better than', t => {
     t.true(betterThan('foo', 'bar')(arr), 'foo is better than bar');
     t.true(betterThan('foo', 'baz')(arr), 'foo is better than baz');
     t.false(betterThan('bar', 'foo')(arr), 'bar is worse than foo');
+    t.end();
+});
+
+test('not directly above', t => {
+    const arr = ['foo', 'bar', 'baz'];
+    t.true(notDirectlyAbove('foo', 'baz')(arr), 'foo is not directly above baz');
+    t.false(notDirectlyAbove('foo', 'bar')(arr), 'foo is directly above bar');
     t.end();
 });
