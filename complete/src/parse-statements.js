3e7e37aad7c4df339e855f9adf8e3616c6c32768
Matthew Riley <matthew@matthewriley.name>
2019-08-28 22:50:03 +1000

    swap order of functions

--- complete/src/parse-statements.js
+++ complete/src/parse-statements.js
@@ -1,16 +1,16 @@
 const statementPredicates = require('./statement-predicates');
 const uniq = names => Array.from(new Set(names));
 
-module.exports = statements => {
-    const parsedStatements = statements.map(parseStatement);
-    const names = uniq(parsedStatements.flatMap(({ names }) => names));
-    const predicates = parsedStatements.flatMap(({ predicates }) => predicates);
-    return [names, predicates];
-};
-
 const parseStatement = statement => {
     const [pattern, ...predicateFuncs] = statementPredicates.find(([pattern]) => pattern.test(statement));
     const names = statement.match(pattern).slice(1);
     const predicates = predicateFuncs.map(predicate => predicate(...names));
     return { names, predicates };
+};
+
+module.exports = statements => {
+    const parsedStatements = statements.map(parseStatement);
+    const names = uniq(parsedStatements.flatMap(({ names }) => names));
+    const predicates = parsedStatements.flatMap(({ predicates }) => predicates);
+    return [names, predicates];
 };
